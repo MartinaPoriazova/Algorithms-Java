cd17db7d00b7efad4f26a15d9bacc89dfe9b8535
mporyazova <mporyazova@yahoo.com>
2022-07-21 23:30:59 +0300

    Exercise: Recursion and Combinatorial Problems

---
+++ src/ExerciseRecursionCombinatorialProblems_04/ReverseArray.java
@@ -0,0 +1,22 @@
+package ExerciseRecursionCombinatorialProblems_04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ReverseArray {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String[] elements = bufferedReader.readLine().split("\\s+");
+
+        printReversedArray(elements, elements.length - 1);
+    }
+
+    private static void printReversedArray(String[] elements, int index) {
+        if (index < 0) {
+            return;
+        }
+        System.out.print(elements[index] + " ");
+        printReversedArray(elements, index - 1);
+    }
+}
